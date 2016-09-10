aa72860097a4a8c2a49f9d9ff1c4a7e6694bec74
王浩东 <2691246603@qq.com>
2016-09-10 09:38:15 +0800

    whd

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -56,15 +56,7 @@
                         </div>
 
                     </li>
-                    <li class="active">
-                        <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">文章管理</span> <span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li><a href="{{url("article")}}">文章列表</a></li>
-                            <li><a href="index_2.html">主页示例二</a></li>
-                            <li><a href="index_3.html">主页示例三</a></li>
-                            <li><a href="index_4.html">主页示例四</a></li>
-                        </ul>
-                    </li>
+                    @include("layouts.menu")
                 </ul>
 
             </div>
